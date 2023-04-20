6e78fc49263ebe299a1173bb44aa34e92afed28f
Soon-cheol <sooncheolchoi7@gmail.com>
2023-04-21 05:31:25 +0900

    링크 수정

--- assets/pub/fantasysquad/assets/event/20170118/comm.js
+++
@@ -1,8 +0,0 @@
-function pop_close() {
-    window.close();
-}
-
-function pop_open() {
-    var popupOption = 'directories=no, toolbar=no, location=no, menubar=no, status=no, scrollbars=yes, resizable=no, left=0, top=0, width=1297, height=800';
-    window.open("newcose.html", "Shotonline", popupOption);
-}
